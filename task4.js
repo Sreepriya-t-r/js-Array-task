9a26954a2e2b34747a1aeb3c93305083b0f782aa
Sreepriya T R <sreepriyatr992@gmail.com>
2025-01-02 03:56:23 +0530

    first commit

---
+++ task4.js
@@ -0,0 +1,101 @@
+//  1. Filter Even numbers in an Array.
+// const arr = [1, 2, 3, 4, 5, 6];
+// const evenNumbers = arr.filter(num => num % 2 === 0);
+// console.log(evenNumbers);
+
+
+// 2. Find index of the largest number
+// const arr = [1, 2, 3, 4, 5, 6];
+// const maxNum = arr.reduce((max, num) => num > max ? num : max, arr[0]);
+// const maxIndex = arr.indexOf(maxNum);
+// console.log(maxIndex); 
+
+//  1. Write a program that uses forEach() to print all elements in the array [1, 2, 3, 4, 5].
+// const arr = [1, 2, 3, 4, 5];
+// arr.forEach(num => console.log(num));
+
+
+//  2. Use forEach() to calculate the sum of all numbers in an array [10, 20, 30, 40].
+// const arr = [10, 20, 30, 40];
+// let sum = 0;
+// arr.forEach(num =>sum +=num);
+// console.log(sum); 
+
+
+//  From an array of ages, filter out those less than 18.
+// const ages = [15, 22, 17, 19, 30, 12];
+// const adults = ages.filter(age => age >= 18);
+// console.log(adults);
+
+
+//  Filter out numbers greater than 10 from an array.
+// const numbers = [5, 12, 8, -20, 3, 15, 7];
+// const filteredNumbers = numbers.filter(num =>num<=10);
+// console.log(filteredNumbers); 
+
+//  1) Find average of elements in an array
+// const arr = [10, 20, 30, 40, 50];
+// const sum = arr.reduce((a, num) => a+num);
+// const average = sum / arr.length;
+// console.log(average); 
+
+
+
+// // 2) filter the multiples of 5 in the given array
+// const numbers = [5, 10, 15, 20, 25, 30, 7, 8];
+// const multiplesOfFive = numbers.filter(num => num % 5 === 0);
+// console.log(multiplesOfFive); 
+
+
+
+// // 1.Use filter() to remove null or undefined values
+// const arr = [1, null, 2, undefined, 3, null, 4];
+// const Arr = arr.filter(value => value!== null && value !== undefined);
+// console.log(Arr);
+
+
+// 2.double every element of an array using map?
+// const arr = [1, 2, 3, 4, 5];
+// const doubledArr = arr.map(num => num * 2);
+// console.log(doubledArr);
+
+
+// 1,find the sum of an array with reduce method
+// const arr = [1, 2, 3, 4, 5];
+// const sum = arr.reduce((a, num) => a+ num);
+// console.log(sum);
+
+
+
+
+
+// 2,print even number from 1 to N
+
+// Filter  the  odd numbers in the array
+
+// Find the average of the number array
+
+// 1)  Filter students who passed
+
+// const students = [
+//   { name: "alice", score: 85 },
+//   { name: "Bob", score: 45 },
+//   { name: "Charlie", score: 75 },
+// ];
+
+// 2)  Convert numbers to boolean
+
+// Const numbers= [ 0, 1,-2, 3,-4,5].
+
+// 2]Given the array of names below, write a program to append the text " is awesome! " to each name and log it to the console:
+// const names = ["Ali,"Bob","Charlie"];
+// [31/12/2024, 17:28] +91 75599 71641: 1.  Let array =[1,2,3,4,5,6,7,8,9,10]
+
+// Find the square of the numbers greater than 5 and cube of other numbers using map?
+
+
+// 2.  Let array = [11,16,24,27,44,51,79,95,100,144,185,204]
+
+// Filter the numbers completely divisible by 4?
+// [31/12/2024, 17:31] Chandana: 1)Print each element of an array to the console
+// 2)Find the sum of all elements in an array
